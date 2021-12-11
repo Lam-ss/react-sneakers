5ea4b1b6c7859f9a9fb2c69c72b615a2fa2f30c0
Lam L <ppoo44ttaa@gmail.com>
2021-12-12 00:23:24 +0300

    feat: add cart

--- src/App.js
+++ src/App.js
@@ -3,11 +3,63 @@
 function App() {
   return (
     <div className="wrapper">
+
+      <div className="overlay">
+        <div className="drawer">
+          <h2>Корзина <img width={40} height={40} className="removeBtn" src="/img/btn-remove.svg" alt="Remove" /></h2>
+          
+
+          <div className="items">
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+              <div className="cartItemText">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+            <div className="cartItem">
+              <div style={{ backgroundImage: 'url(/img/sneakers/1.jpg)' }} className="cartItemImg"></div>
+              <div className="cartItemText">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+            </div>
+          </div>
+
+
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li>
+                <span>Налог 5%</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">Оформить заказ
+              <img src="/img/arrow.svg" alt="Arrow" />
+            </button>
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
       <header>
         <div className="headerLeft">
           <img width={40} height={40} src="/img/logo.png" />
           <div>
-            <h3>React Sneakers</h3>
+            <h3>React Sneakers</h3>   
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
@@ -23,10 +75,20 @@ function App() {
         </ul>
       </header>
       <div className="content">
-        <h1>Все кроссовки</h1>
+        <div className="content-item">
+          <h1>Все кроссовки</h1>
+          <div className="search-blok">
+            <img src="/img/search.svg" alt="Search" />
+            <input placeholder="Поиск ... " />
+          </div>
+        </div>
+
 
         <div className="sneakers">
           <div className="card">
+            <div className="favorite">
+              <img src="/img/heart-unliked.svg" alt="Unliked" />
+            </div>
             <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="cardBottom">
